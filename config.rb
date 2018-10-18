3db9c0d2404dd705840208f882e29a8f0159434b
EasterPeanut <pien_van_dalen@hotmail.com>
2018-10-18 08:10:08 +0200

    Add gems and activate some of them

--- config.rb
+++ config.rb
@@ -4,26 +4,29 @@
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
+activate :livereload
+# activate :i18n
+# activate :search
 
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
 # Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+page "/*.xml", layout: false
+page "/*.json", layout: false
+page "/*.txt", layout: false
 
 # With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+# page "/path/to/file.html", layout: "other_layout"
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
 
 # proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
+#   "/this-page-has-no-template.html",
+#   "/template-file.html",
 #   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
+#     which_fake_page: "Rendering a fake page with a local variable"
 #   },
 # )
 
@@ -33,14 +36,20 @@ page '/*.txt', layout: false
 
 # helpers do
 #   def some_helper
-#     'Helping'
+#     "Helping"
 #   end
 # end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_html
+  activate :minify_javascript
+
+  # Append a hash to asset urls (make sure to use the url helpers)
+  activate :asset_hash
+
+  activate :asset_host, :host => "//legobox.io"
+end
