f4c89c44695d184cae5dbf9bc22865a6c2a38270
EasterPeanut <pien_van_dalen@hotmail.com>
2018-10-21 17:45:24 +0200

    Add global configs, start with head of layout and make bricks

--- config.rb
+++ config.rb
@@ -1,11 +1,34 @@
+# Global settings
+
+config[:company_address] = "Gouwe 102"
+config[:company_country] = "Nederland"
+config[:company_email] = "info@legobox.io"
+config[:company_lat] = "53.000986"
+config[:company_long] = "6.538577"
+config[:company_phone_number] = "+31 65 777 5633"
+config[:company_placename] = "Assen"
+config[:company_postal_code] = "9406 GS"
+config[:company_name] = "Legobox"
+config[:host] = "localhost"
+config[:meta_csp_settings] = " img-src https://*; child-src 'none';"
+config[:meta_geo_position] = config[:company_lat] + ";" + config[:company_long]
+config[:meta_geo_placename] = config[:company_placename] + ", " + config[:company_country]
+config[:meta_geo_region] = "NL-DR"
+config[:root_locale] = :nl
+config[:locales] = [:nl, :en]
+config[:sm_fb_app_id] = "123456789"
+config[:sm_twitter_site_account] = "@legobox"
+config[:sm_twitter_user_account] = "@legoman"
+
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
+activate :directory_indexes
 activate :livereload
-# activate :i18n
+activate :i18n, mount_at_root: config[:root_locale], :langs => config[:locales]
 # activate :search
 
 # Layouts
@@ -34,16 +57,24 @@ page "/*.txt", layout: false
 # Methods defined in the helpers block are available in templates
 # https://middlemanapp.com/basics/helper-methods/
 
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  # Get full locale (eg. nl_NL)
+  def full_locale(lang = I18n.locale)
+    case lang
+      when :en
+        "en_US"
+      else
+        "#{lang.downcase}_#{lang.upcase}"
+    end
+  end
+end
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
 configure :build do
+  config[:host] = "https://www.legobox.io"
+
   activate :minify_css
   activate :minify_html
   activate :minify_javascript
