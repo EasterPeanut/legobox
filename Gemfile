ff90b9c1d66d95791bef53f71e2f7b835273c8e9
EasterPeanut <pien_van_dalen@hotmail.com>
2018-11-10 10:12:39 +0100

    Add sprockets and add navigation toggle

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem "middleman", "~> 4.2"
 gem "middleman-autoprefixer", "~> 2.7"
 gem "middleman-livereload", "~> 3.4"
 gem "middleman-minify-html", "~> 3.4.1"
+gem "middleman-sprockets", "~> 4.1.1"
 gem "middleman-syntax", "~> 3.0.0"
 gem "tzinfo-data", platforms: [:mswin, :mingw, :jruby]
 gem "wdm", "~> 0.1", platforms: [:mswin, :mingw]
--- source/javascripts/_header.js
+++ source/javascripts/_header.js
@@ -1,5 +1,11 @@
 let navToggleButton = document.getElementById("navToggleButton");
+let navList = document.getElementById("navList");
+
+navList.classList.remove("expanded")
+navList.classList.add("collapsed")
 
 navToggleButton.onclick = function () {
   navToggleButton.classList.toggle("active");
+  navList.classList.toggle("collapsed")
+  navList.classList.toggle("expanded")
 };
